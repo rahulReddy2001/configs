3641be5b128adfb4b7f6f72198e2b7a380b64e00
rahulReddy2001 <skondlap@purdue.edu>
2025-06-05 20:00:28 +0530

    Added node installation for LSPs

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 sudo apt update && sudo apt install -y \
   tmux neovim git curl wget unzip \
-  build-essential python3-pip
+  build-essential python3-pip node
 
 # Clone TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
