e493bb7f8d376a3eb3d6abb323550cb40af9834b
rahulReddy2001 <91095387+rahulReddy2001@users.noreply.github.com>
2025-06-13 18:20:00 +0530

    Updated links.sh
    Removed git config link

--- scripts/links.sh
+++ scripts/links.sh
@@ -9,7 +9,6 @@ mkdir -p "$HOME/.config"
 
 ln -sf "$SCRIPT_DIR/tmux/.tmux.conf" ~/.tmux.conf
 ln -sf "$SCRIPT_DIR/nvim" ~/.config/nvim
-ln -sf "$SCRIPT_DIR/git/.gitconfig" ~/.gitconfig
-# Add more as needed
+
 
 echo "✅ Done."
