9d5d31aa92a997bd01773b6d1ccadbeb27e9962b
skondlap <skondlap@node.skondlap-270315.serverless-2-pg0.cloudlab.umass.edu>
2025-09-17 02:46:28 +0000

    Made changes to the lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -38,8 +38,13 @@ return {
         capabilities = capabilities,
       })
       vim.keymap.set('n', 'K', vim.lsp.buf.hover,{})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition,{})
+      vim.keymap.set('n', 'gi', vim.lsp.buf.definition,{})
       vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action,{})
+      vim.keymap.set('n', '<leader>ge', vim.diagnostic.open_float, {})
+      vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, {})
+      vim.keymap.set('n', ']d', vim.diagnostic.goto_next, {})
+      vim.keymap.set('n', 'gb', '<C-o>', { desc = "Go back after definition" })
+
     end
   }
 }
